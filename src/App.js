982c099f9edbf03ca4d48cceef8c11503e6671d0
McAuley Penney <jacobmpenney@gmail.com>
2022-12-07 10:04:17 -0700

    feat: init interface demo
    - rm files from default app that aren't used right
      now
    
    - App.js
    	- add input fields for
    		- Exposure duration
    		- Number of exposures
    
    	- add buttons for
    		- pointing device
    		- starting observation

--- src/App.js
+++ src/App.js
@@ -1,35 +1,98 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import FormControl from '@mui/material/FormControl';
+import InputAdornment from '@mui/material/InputAdornment';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
 
-function App() {
-    const [currentTime, setCurrentTime ] = useState(0);
+// https://mui.com/material-ui/material-icons/
+import LocationSearchingIcon from '@mui/icons-material/LocationSearching';
+import RocketIcon from '@mui/icons-material/RocketLaunch';
 
-    useEffect(() => {
-	fetch( '/time' ).then( res => res.json()).then( data => {
-	    setCurrentTime( data.time );
+
+export default function InputAdornments() {
+	const [values, setValues] = React.useState({
+		amount: '',
+		password: '',
+		weight: '',
+		weightRange: '',
 	});
-    }, []);
-    
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-	<p> The current time is {currentTime}. </p>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+	const handleChange = (prop) => (event) => {
+		setValues({ ...values, [prop]: event.target.value });
+	};
+
+
+	return (
+		<>
+			<Box
+				component="form"
+				sx={{
+					'& .MuiTextField-root': { m: 1, width: '10ch' },
+				}}
+				noValidate
+				autoComplete="off"
+			>
+				<div style={{
+					display: 'flex',
+					alignItems: 'center',
+				}}>
+					<text>
+						Time
+						<span style={{ marginLeft: '7.6rem' }}>&nbsp;</span>
+						0/
+					</text>
+					<FormControl sx={{ m: 1, width: '17ch' }} variant="outlined">
+						<OutlinedInput
+							id="outlined-adornment-weight"
+							value={values.weight}
+							size="small"
+							onChange={handleChange('weight')}
+							endAdornment={<InputAdornment position="end">Second(s)</InputAdornment>}
+							aria-describedby="outlined-weight-helper-text"
+							inputProps={{
+								'aria-label': 'weight',
+							}}
+						/>
+					</FormControl>
+				</div>
+
+				<div style={{
+					display: 'flex',
+					alignItems: 'center',
+				}}>
+					<text>
+						Exposures
+						<span style={{ marginLeft: '5rem' }}>&nbsp;</span>
+						1 of
+					</text>
+					<TextField
+						id="outlined-number"
+						label="Exposures"
+						type="number"
+						size="small"
+						InputLabelProps={{
+							shrink: true,
+						}}
+					/>
+				</div>
+			</Box>
+
+			<Stack direction="row" spacing={5}>
+				<Button
+					variant="contained"
+					sx={{}}
+					startIcon={<LocationSearchingIcon />}
+				>
+					Point
+				</Button>
+
+				<Button variant="contained" startIcon={<RocketIcon />}>
+					Start
+				</Button>
+			</Stack>
+		</>
+	)
+}
