6e253f465d035dcd78e662d5c11954a3bc6abe0d
Linh Nhat Nguyen <37945098+nhatlinh300401@users.noreply.github.com>
2022-12-06 21:16:46 -0700

    Update views.py

--- api/main/views.py
+++ api/main/views.py
@@ -1,7 +1,24 @@
-from main import app
-#from core.models import db
-import time
+from flask import Flask, request
+from flask_cors import CORS
+import time, requests
 
-@app.route('/time')
+
+# import other routing and backend files
+#import testpage2
+
+cors = CORS()
+
+app = Flask(__name__)
+cors.init_app(app)
+
+@app.route('/planets')
 def get_current_time():
     return { 'time': time.time() }
+
+@app.route('/submit', methods=['POST'], strict_slashes=False)
+def submit_planet():
+    planet_data = request.get_json()
+
+    r = requests.post("http://localhost:5001/request", data={planet_data})
+
+    return {"planet" : planet_data['name']}
