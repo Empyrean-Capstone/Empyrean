f2d9e3717829ba1e96b91ea1f584b6e123d702fa
McAuley Penney <jacobmpenney@gmail.com>
2023-02-09 12:42:37 -0700

    refactor(api): register observe blueprint
    - format touched files
    - add get_env_variable docstring
    - refactor get_env_variable fn according to
      pylint suggestions
    - remove unused getcwd function

--- api/main/__init__.py
+++ api/main/__init__.py
@@ -1,18 +1,27 @@
-from flask import Flask
 import os
+from flask import Flask
+from .observe.views import observe
 
-basedir = os.getcwd()
-
-app = Flask( __name__ )
+app = Flask(__name__)
 
 
 # config helpers
-def get_env_variable( name ):
+def get_env_variable(name: str) -> str | None:
+    """
+    Return Flask environment vars.
+
+    Args:
+        name (str): environment variable to retrieve.
+
+    Returns:
+        str or None: environment var or exception
+    """
     try:
         return os.environ.get(name)
-    except KeyError:
-        message = "Expected env variable '{}' not set.".format( name )
-        raise Exception( message )
+    except KeyError as exception:
+        message = f"Expected env variable '{name}' not set."
+        raise Exception(message) from exception
+
 
 # Set variables
 POSTGRES_URL = get_env_variable("POSTGRES_URL")
@@ -20,5 +29,8 @@ POSTGRES_USER = get_env_variable("POSTGRES_USER")
 POSTGRES_PW = get_env_variable("POSTGRES_PW")
 POSTGRES_DB = get_env_variable("POSTGRES_DB")
 
-import main.views as views
-#app.register_blueprint( example_blueprint, url_prefix="/buyer" )
+
+app.register_blueprint(observe)
+
+
+from . import views
