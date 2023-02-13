cf29d4ff359caee08e171730218b15b2f55fa932
jpirkl743 <jap743@nau.edu>
2023-02-13 15:44:36 -0700

    added basic login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
@@ -5,8 +6,8 @@ import { About } from './pages/about/about';
 import {Contact} from './pages/contact/contact'
 import {Observation} from './pages/observation'
 import {LogsheetPage} from './pages/logsheet'
-import React from 'react';
 import {SocketContext, socket} from './context/socket';
+import { Login } from './pages/login'
 
 function App() {
 
@@ -15,10 +16,11 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path='/' element={<Layout/>}>
-						<Route index element={<Observation/>}/>
+						<Route index element={<Login/>}/>
 						<Route path="about" element={<About/>}/>
 						<Route path="contact" element={<Contact/>}/>
 						<Route path="logsheet" element={<LogsheetPage/>}/>
+						<Route path="observation" element={<Observation/>}/>
 					</Route>
 				</Routes>
 			</BrowserRouter>
