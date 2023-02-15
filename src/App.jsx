d96357252c6ea9c73d143d90e71a0fdc29b059da
jpirkl743 <jap743@nau.edu>
2023-02-15 15:44:54 -0700

    update init

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { About } from './pages/about/about';
-import {Contact} from './pages/contact/contact'
-import {Observation} from './pages/observation'
-import {Logsheet} from './pages/logsheet'
+import { Contact } from './pages/contact/contact'
+import { Observation } from './pages/observation'
+import { LogsheetPage } from './pages/logsheet'
 import { Login } from './pages/login'
 
 
@@ -16,7 +16,7 @@ function App() {
 					<Route index element={<Observation/>}/>
 					<Route path="about" element={<About/>}/>
 					<Route path="contact" element={<Contact/>}/>
-					<Route path="logsheet" element={<Logsheet/>}/>
+					<Route path="logsheet" element={<LogsheetPage/>}/>
 					<Route path="login" element={<Login/>}/>
 				</Route>
 			</Routes>
