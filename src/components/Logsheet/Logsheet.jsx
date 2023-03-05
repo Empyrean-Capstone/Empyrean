c0217b839e8df74ace349182e4979f5c8e75d905
koseward <kos34@nau.edu>
2023-03-05 16:58:51 -0700

    Restored SIMPAD usage

--- src/components/Logsheet/Logsheet.jsx
+++ src/components/Logsheet/Logsheet.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import React, {useState, useEffect, useContext} from 'react';
-=======
-import React, {useState, useEffect} from 'react';
->>>>>>> main
 import './style.css'
 
 import Box from '@mui/material/Box';
