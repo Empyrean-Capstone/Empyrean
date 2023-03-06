085d63ba4a902caf32673fdf6d43e82f68ddc587
Linh Nhat Nguyen <37945098+nhatlinh300401@users.noreply.github.com>
2023-03-05 17:01:17 -0700

    Update logsheet.py

--- api/main/models/logsheet.py
+++ api/main/models/logsheet.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class Logsheet(db.Model):
     id = db.Column( db.Integer, primary_key = True )
     date_created = db.Column( db.DateTime, default=datetime.utcnow )
-    staring_id = db.Column( db.Integer )
+    starting_id = db.Column( db.Integer )
     date_number = db.Column( db.String )
 
     def __init__(self) -> None:
