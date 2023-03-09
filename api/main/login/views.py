ce1597bf7b0a096f75bf5c86920158df97c63f3f
jpirkl743 <78187418+jpirkl743@users.noreply.github.com>
2023-03-08 20:38:15 -0700

    Update views.py

--- api/main/login/views.py
+++ api/main/login/views.py
@@ -11,20 +11,18 @@ def post_login():
     username_req = login_input["username"]
     password_req = login_input["password"]
 
-    if( db.session.query( func.count(User)) == 0) {
+    if db.session.query(func.count(User)) == 0:
     	user_obj = User(username_req, password_req)
 
     	db.session.add(user_obj)
     	db.session.commit()
 
 	return {"response": True}
-    }
-    else {
-    	check_pass = db.session.query(User.password).\
-		filter(User.username == username_req)
+		
+    check_pass = db.session.query(User.password).\
+	filter(User.username == username_req)
 
-	if( check_pass == password_req ) {
+    if check_pass == password_req:
 		return {"response": True}
-	}
-	
+
     return {"response": False}
