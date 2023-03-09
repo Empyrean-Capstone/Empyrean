3e60fec762d65fe1361e4b859ae59e3b7aaa4679
McAuley Penney <jacobmpenney@gmail.com>
2023-03-09 08:21:29 -0700

    feat, refactor(status/views):
    - change enum class to dictionary for simplicity
    - rename "get_id" -> "get_instrument_id" for
      clarity
    - format, organize
    
    - add ZWOASI camera to list of cameras to give
      "camera" type status too.
    
      - This is temporary. Later, we will implement
    	an interface for instruments that will handle
    	this

--- api/main/status/views.py
+++ api/main/status/views.py
@@ -1,50 +1,42 @@
-
-
 from flask import request, jsonify
 from main import db, app
 from .. import sio
 from . import status
 from ..models import Status, Instrument
-from enum import Enum
 
-class SpectrographStatus( Enum ):
-    Mirror = 0
-    LED = 1
-    ThAr = 2
-    Tungsten = 3
 
-@sio.on( 'get_id' )
-def get_object_id( object_type ):
+SPECTROGRAPH_STATUS = {"Mirror": 0, "LED": 1, "ThAr": 2, "Tungsten": 3}
 
+
+@status.get("/index")
+def index():
+    result = Status.query.all()
+    for index in range(len(result)):
+        result[index] = result[index].serialize()
+    return result
+
+
+@sio.on("get_instrument_id")
+def get_instrument_id(instrument_name):
     # make query to recieve the id of the requested object
-    instrument = Instrument.query.filter_by( instrumentName = object_type ).first()
+    instrument = Instrument.query.filter_by(instrumentName=instrument_name).first()
 
     # if no result, define a new object, with it's statuses
     if instrument == None:
-        id = define_status( object_type )
-    else:
-        id = instrument.instrumentId
+        return define_status(instrument_name)
 
-    # return the id to the object
-    return id
+    return instrument.instrumentId
 
-@sio.on( 'spectrograph_made_change' )
-def change_spctrograph_mode( mode, id ):
 
+@sio.on("spectrograph_made_change")
+def change_spctrograph_mode(mode, id):
     # update each of their statuses
     for key, value in mode.items():
-        update = Status.query.filter_by( instrumentID = id, statusName = SpectrographStatus(key).name )
+        update = Status.query.filter_by(instrumentID=id, statusName=SPECTROGRAPH_STATUS[key])
         update.statusValue = "On" if value == 1 else "Off"
 
-    #commit the changes
+    # commit the changes
     db.session.commit()
-
-@status.get('/index')
-def index():
-    result = Status.query.all()
-    for index in range(len( result )): 
-        result[ index ] = result[ index ].serialize()
-    return result
 
 
 @sio.on("update_status")
@@ -68,30 +60,32 @@ def update_request_form():
     sio.emit("enable_request_form")
 
 
-"""
-Defines the camera Instrument, 
-then gives statuses based on type,
-Then saves this information to the database
-"""
-def define_status( object_name ):
-    new_camera = Instrument( object_name )
-    db.session.add( new_camera )
+def define_status(instrument_name):
+    """
+    Defines the camera Instrument,
+    then gives statuses based on type,
+    Then saves this information to the database
+    """
+    new_camera = Instrument(instrument_name)
+    db.session.add(new_camera)
     db.session.commit()
 
-    instrument_id = Instrument.query.filter_by( instrumentName = object_name ).first().instrumentId # finds the newly created object, and gathers its id 
+    instrument_id = (
+        Instrument.query.filter_by(instrumentName=instrument_name).first().instrumentId
+    )  # finds the newly created object, and gathers its id
 
     new_db_objects = []
-    match object_name:
-        case "camera":
-            new_db_objects.append( Status( instrumentID=instrument_id, statusName="Camera", statusValue="Idle" ) )
-            new_db_objects.append( Status( instrumentID=instrument_id, statusName="currentExposure", statusValue="0" ) )
-            new_db_objects.append( Status( instrumentID=instrument_id, statusName="remainingExposure", statusValue="0" ) )
+    match instrument_name:
+        case ("camera", "ZWO ASI120MM-S"):
+            new_db_objects.append(Status(instrumentID=instrument_id, statusName="Camera", statusValue="Idle"))
+            new_db_objects.append(Status(instrumentID=instrument_id, statusName="currentExposure", statusValue="0"))
+            new_db_objects.append(Status(instrumentID=instrument_id, statusName="remainingExposure", statusValue="0"))
         case "spectrograph":
-            new_db_objects.append( Status( instrument_id, "Mirror", "Off" ))
-            new_db_objects.append( Status( instrument_id, "LED", "Off" ))
-            new_db_objects.append( Status( instrument_id, "ThAr", "Off" ))
-            new_db_objects.append( Status( instrument_id, "Tungsten", "Off" ))
+            new_db_objects.append(Status(instrument_id, "Mirror", "Off"))
+            new_db_objects.append(Status(instrument_id, "LED", "Off"))
+            new_db_objects.append(Status(instrument_id, "ThAr", "Off"))
+            new_db_objects.append(Status(instrument_id, "Tungsten", "Off"))
     for status in new_db_objects:
-        db.session.add( status )
+        db.session.add(status)
     db.session.commit()
     return instrument_id
