de8ebea14c3ae98b4c1640eac5c2fb7b16e4aa29
McAuley Penney <jacobmpenney@gmail.com>
2023-03-16 14:19:28 -0700

    feat, fix: Initialize use of Flask sessions
    The application requires authentication and user
    functionality. The most expedient means that also
    satisfactorily fulfilled our requirements was
    Flask sessions.
    
    In the course of implementing them, update the
    login API to provide login, logout, registration,
    and session validation routes and with
    docstrings. These routes return proper HTTP
    status codes.
    
    Note: This commit also contains merges with main
    	  because this commit was made while
    	  squashing a development branch using soft
    	  reset

--- api/main/login/views.py
+++ api/main/login/views.py
@@ -1,4 +1,14 @@
-from flask import request
+"""
+Routes related to user authentication.
+
+http status references:
+    1. https://httpwg.org/specs/rfc9110.html#status.codes
+    2. https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+"""
+
+
+from flask import request, session
+
 from main import db
 from . import login
 from ..models.user import User
@@ -6,23 +16,101 @@ from ..models.user import User
 
 @login.post("/")
 def post_login():
-    login_input: dict = request.get_json()
+    """
+    Authenticate the user using their username and password.
 
+    Returns:
+        200: The user was successfully logged in
+        401: User credentials did not match an existing user
+    """
+    login_input: dict = request.get_json()
     username_req = login_input["username"]
     password_req = login_input["password"]
 
-    if db.session.query(func.count(User)) == 0:
-    	user_obj = User(username_req, password_req)
+    if User.query.count() == 0:
+        return "unauthorized", 401
 
-    	db.session.add(user_obj)
-    	db.session.commit()
+    try:
+        cur_user = User.query.filter_by(username=username_req).first()
+        if password_req == cur_user.password:
+            session["userid"] = str(cur_user.id)
+            return "success", 200
 
-	return {"response": True}
-		
-    check_pass = db.session.query(User.password).\
-	filter(User.username == username_req)
+        else:
+            return "unauthorized", 401
 
-    if check_pass == password_req:
-		return {"response": True}
+    except (AttributeError, TypeError):
+        return "unauthorized", 401
 
-    return {"response": False}
+
+@login.post("/logout/")
+def logout():
+    """
+    Log the user out, ending their user session.
+
+    Returns:
+        200: The user was successfully logged out
+        404: endpoint is valid but the requested
+             resource could not be found
+    """
+    res = session.pop("userid", None)
+
+    if res is None:
+        return "not found", 404
+
+    return "success", 200
+
+
+@login.post("/register/")
+def register_new_user():
+    """
+    Register a new user using their username and password.
+
+    Returns:
+        201: Registration was successful and a new
+             user was created.
+
+        409 (Conflict): User, found via username, already
+                        exists
+    """
+
+    def commit_user():
+        user_obj = User(username_req, password_req)
+
+        db.session.add(user_obj)
+        db.session.commit()
+
+        return "created", 201
+
+    reg_input: dict = request.get_json()
+    username_req = reg_input["username"]
+    password_req = reg_input["password"]
+
+    if User.query.count() == 0:
+        return commit_user()
+
+    cur_user = User.query.filter_by(username=username_req).first()
+
+    if cur_user is None:
+        return commit_user()
+
+    else:
+        return "user exists", 409
+
+
+@login.post("/validate/")
+def validate_session():
+    """
+    Check if the user has a valid session.
+
+    Returns:
+        200: user has a valid session.
+        404: endpoint is valid but the requested
+             resource could not be found
+    """
+    userid = session.get("userid")
+
+    if userid is None:
+        return "not found", 404
+
+    return "success", 200
