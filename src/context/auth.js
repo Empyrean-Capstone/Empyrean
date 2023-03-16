1482f8cbd43dc0c6e15a0cb09cd3552673d6c77e
McAuley Penney <jacobmpenney@gmail.com>
2023-03-16 14:22:37 -0700

    feat, fix: finalize initial frontend authentication logic
    The application requires authentication and user
    functionality. With the backend having a basic
    implementation of flask sessions, we can ply them
    on the frontend to authenticate a user.
    
    - add protection to certain routes, whereby the
      user must have a valid session to enter
    
      - add loading overlay to indicate pending page
        change during validation API call when
    	attempting to access protected routes
    
    - fix the UI of the current login page
    	- add toasts to indicate login failure and
    	  registration success and failure
    
    	- add optional password invisibility
    
    	- add a registration button
    
    - add a logout link to the sidebar
    - omit the sidebar on the login page
    
    Note: This commit also contains merges with main
             because this commit was made while
             squashing a development branch using soft
             reset

--- src/context/auth.js
+++
@@ -1,5 +0,0 @@
-import {io} from "socket.io-client";
-import React from 'react';
-
-export const auth = False;
-export const AuthContext = React.createContext();
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import bcrypt from 'bcrypt';
 import axios from 'axios';
+
 import './style.css'
 
-import { AuthContext, auth } from './context/auth';
 import { useNavigate } from "react-router-dom";
-
-import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Unstable_Grid2'
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import MuiAlert from '@mui/material/Alert';
 import Paper from '@mui/material/Paper';
-
+import Snackbar from '@mui/material/Snackbar';
+import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { LoadingButton } from "@mui/lab"
+import { styled } from '@mui/material/styles';
+
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 const Item = styled(Paper)(({ theme }) => ({
 	backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -20,104 +30,203 @@ const Item = styled(Paper)(({ theme }) => ({
 	color: theme.palette.text.secondary,
 }));
 
-const handleChange = (prop) => (event) => {
-        setValues({ ...values, [prop]: event.target.value });};
 
 function Login() {
 	const navigate = useNavigate();
 
-	const { auth, setAuth } = React.useContext(LanguageContext);
-	
 	const [isLoading, setLoading] = React.useState("");
+	const [authState, setAuthState] = React.useState({
+		set: false,
+		msg: "",
+		severity: ""
+	});
 
 	const [values, setValues] = React.useState({
 		username: "",
 		password: "",
 	});
 
+	const handleChange = (prop) => (event) => {
+		setValues({ ...values, [prop]: event.target.value });
+		setAuthState("")
+	};
+
+	const [showPassword, setShowPassword] = React.useState(true);
+
 	const props = { values, handleChange }
 
 	return (
-		<div>
-			<Item
-				sx={{ width: 400,
-					height: 300,
-					display: 'center',
-					mt: '40%',
-					ml: '40%',
+		<Grid
+			container
+			spacing={0}
+			display="flex"
+			alignItems="center"
+			justifyContent="center"
+			sx={{ backgroundColor: "#ebeef2" }}
+			style={{ minHeight: '100vh' }
+			}
+		>
+			<Grid item>
+				<Item
+					sx={{
+						width: 400,
+						height: '20%',
 					}}
-			>
-
-				<TextField
-					sx={{mt: 2, mb: 2}}
-					required
-					fullWidth
-					id="outlined"
-					value={values.username}
-					size="small"
-					onChange={handleChange("username")}
-					label="Username"
-					type="text"
-					InputLabelProps={{
-						shrink: true,
-					}}
-				/>
-
-				<TextField
-					sx={{mt: 2, mb: 2}}
-					required
-					fullWidth
-					id="outlined"
-					value={values.password}
-					size="small"
-					onChange={handleChange("password")}
-					label="Password"
-					type="text"
-					InputLabelProps={{
-						shrink: true,
-					}}
-				/>
-
-				<LoadingButton
-					sx={{mt: 2, mb: 2}}
-					variant="contained"
-					type="submit"
-					onClick={() => {
-						setLoading("Login");
-
-						const salt = bcrypt.genSaltSync(0);
-						values.password = bcrypt.hashSync(values.password, salt);
-
-						const initLogin = async (values) => {
-							try {
-								const resp = await axios.post(`http://localhost:5000/auth_login/`, values);
-								
-								setAuth( resp );
-								
-								if( resp ) {
-									console.log("Logged In");									
-									navigate("/observation");
-								}
-								
-								console.log("Wrong Password");
-								
-							} catch (err) {
-								console.error(err);
-							}
-
-							setLoading("");
-						};
-
-						initLogin(props.values);
-					}}
-					loadingPosition="center"
-					loading={isLoading === "Login"}
-					disabled={isLoading !== "" && isLoading !== "Login"}
 				>
-					Login
-				</LoadingButton>
-			</Item>
-		</div>
+
+					<TextField
+						sx={{ mt: 2, mb: 2 }}
+						required
+						fullWidth
+						disabled={isLoading !== ""}
+						type="text"
+						key={"Username"}
+						label={"Username"}
+						size="small"
+						id="outlined"
+						value={values["username"]}
+						onChange={handleChange("username")}
+						InputLabelProps={{
+							shrink: true,
+						}}
+					/>
+
+					<TextField
+						sx={{ mt: 2, mb: 2 }}
+						required
+						fullWidth
+						disabled={isLoading !== ""}
+						type={showPassword ? 'text' : 'password'}
+						key={"Password"}
+						label="Password"
+						size="small"
+						id="outlined-start-adornment"
+						value={values["password"]}
+						onChange={handleChange("password")}
+						InputProps={{
+							endAdornment: <InputAdornment position="end">
+								<IconButton
+									aria-label="toggle password visibility"
+									onClick={() => setShowPassword((show) => !show)}
+									onMouseDown={(event) => {
+										event.preventDefault();
+									}}
+									edge="end"
+								>
+									{showPassword ? <VisibilityOff /> : <Visibility />}
+								</IconButton>
+							</InputAdornment>
+						}}
+						InputLabelProps={{
+							shrink: true,
+						}}
+					/>
+
+					<Stack
+						alignItems="center"
+						justifyContent="center"
+						direction="row" spacing={3}
+					>
+						<LoadingButton
+							sx={{ mt: 2, mb: 2 }}
+							variant="contained"
+							type="submit"
+							onClick={() => {
+								setLoading("login");
+
+								//  const salt = bcrypt.genSaltSync(0);
+								//  values.password = bcrypt.hashSync(values.password, salt);
+
+								const attemptLogin = async (values) => {
+									try {
+										let auth_res = await axios.post(
+											`http://localhost:5000/auth_login/`,
+											values,
+											{
+												withCredentials: true
+											}
+										)
+										if (auth_res.status === 200) {
+											navigate("/observation");
+										}
+									}
+									catch (err) {
+										setAuthState({
+											set: true,
+											msg: "Username or password are incorrect",
+											severity: "error"
+										})
+									}
+									finally {
+										setLoading("")
+									}
+								};
+
+								attemptLogin(props.values);
+							}}
+							loadingPosition="center"
+							loading={isLoading === "login"}
+							disabled={isLoading === "registration"}
+						>
+							Login
+						</LoadingButton>
+
+						<LoadingButton
+							sx={{ mt: 2, mb: 2 }}
+							variant="outlined"
+							type="submit"
+							onClick={() => {
+								setLoading("registration");
+								const attemptRegistration = async (values) => {
+									try {
+
+										let reg_res = await axios.post(
+											`http://localhost:5000/auth_login/register/`,
+											values,
+											{
+												withCredentials: true
+											}
+										)
+										if (reg_res.status === 201) {
+											setLoading("");
+
+											setAuthState({
+												set: true,
+												msg: `user "${values.username}" sucessfully registered`,
+												severity: "success"
+											})
+										}
+									} catch (err) {
+										setAuthState({
+											set: true,
+											msg: `user "${values.username}" already exists`,
+											severity: "error"
+										})
+									}
+									finally {
+										setLoading("")
+									}
+								};
+
+								attemptRegistration(props.values);
+							}}
+							loadingPosition="center"
+							loading={isLoading === "registration"}
+							disabled={isLoading === "login"}
+						>
+							Register
+						</LoadingButton>
+					</ Stack>
+
+					<Snackbar open={authState.set} anchorOrigin={{ vertical: "top", horizontal: "center" }}>
+						<Alert variant="standard" severity={authState.severity} sx={{ width: '100%' }}>
+							{authState.msg}
+						</Alert>
+					</Snackbar>
+
+				</Item >
+			</Grid>
+		</Grid >
 	)
 }
 
