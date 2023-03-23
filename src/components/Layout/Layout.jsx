32cd6460e22f90a9c964ea4fcacb9b7af1dfb9f2
McAuley Penney <jacobmpenney@gmail.com>
2023-03-23 07:28:56 -0700

    feat: add fancy navigation menu
    The navigation menu did not have a great deal of
    styling. This commit initializes a react version
    of the navigation menu with enough styling that
    we won't have to think about it for a while, while
    we focus on finalizing functionality.

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,11 +4,93 @@ import { Link, Outlet, useNavigate } from 'react-router-dom'
 
 import './style.css'
 
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import { styled, ThemeProvider, createTheme } from '@mui/material/styles';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+// icons
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import FilterCenterFocusIcon from '@mui/icons-material/FilterCenterFocus';
+import Home from '@mui/icons-material/Home';
+import LogoutIcon from '@mui/icons-material/Logout';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+
+
+function Footer() {
+	return (
+		<>
+			<hr id="footer-line-break" />
+			<footer id='footer-section'>
+				<p id="left-info">
+					Designed by students from NAU's CEIAS capstone program
+				</p>
+				<span>
+					V0.0.2
+				</span>
+				<ul id="right-info">
+					<li className="footer-nav-link"><Link to="/about">About</Link></li>
+					<li className="footer-nav-link"><Link to="/contact">Contact</Link></li>
+				</ul>
+			</footer>
+		</>
+	)
+}
+
+
+function CreateNavItems({ items }) {
+	let linkList = items.map((linkObj) => {
+		const { text, icon, onClick } = linkObj;
+
+		return (
+			<ListItem key={text} onClick={onClick}>
+				<ListItemButton dense sx={{ py: 0, minHeight: 27, color: 'rgba(255,255,255,.8)' }}>
+					<ListItemIcon sx={{ color: 'inherit' }}>
+						{icon}
+					</ListItemIcon>
+
+					<ListItemText
+						primary={text}
+						primaryTypographyProps={{
+							align: "left",
+							fontSize: 12,
+							fontWeight: 'medium',
+						}}
+					/>
+
+				</ListItemButton>
+			</ListItem >
+		)
+	})
+
+	return linkList
+}
+
+
+const CustomNav = styled(List)({
+	'& .MuiListItemButton-root': {
+		paddingLeft: 24,
+		paddingRight: 24,
+	},
+	'& .MuiListItemIcon-root': {
+		minWidth: 0,
+		marginRight: 16,
+	},
+	'& .MuiSvgIcon-root': {
+		fontSize: 20,
+	},
+});
+
 function Layout() {
+	const drawerWidth = 240
 	const navigate = useNavigate();
 
 	let [username, setUsername] = useState("")
-
 	useEffect(() => {
 		(async () => {
 			const res = await axios.get(
@@ -18,7 +100,7 @@ function Layout() {
 				})
 
 			if (res.status === 200) setUsername(res.data)
-				else console.error(res.status)
+			else console.error(res.status)
 		})();
 
 		return () => {
@@ -26,73 +108,172 @@ function Layout() {
 		};
 	}, [setUsername]);
 
+	const userItems = [
+		{
+			"text": 'Log Out',
+			"icon": <LogoutIcon />,
+			"onClick": () => {
+				const logout = async () => {
+					try {
+						let logout_res = await axios.post(
+							`http://localhost:5000/auth_login/logout/`,
+							null,
+							{
+								withCredentials: true
+							})
+
+						if (logout_res.status === 200) {
+							navigate("/");
+						}
+					} catch (err) {
+						console.error(err);
+					}
+				};
+
+				logout()
+			}
+		},
+		{
+			"text": 'Register a New User',
+			"icon": <PersonAddIcon />,
+			"onClick": () => navigate('/registration'),
+		},
+	];
+
+	const navItems = [
+		{
+			"text": 'Request Observations',
+			"icon": <FilterCenterFocusIcon />,
+			"onClick": () => navigate('/observation'),
+		},
+		{
+			"text": 'Explore Logs',
+			"icon": <AssignmentIcon />,
+			"onClick": () => navigate('/logsheet'),
+		},
+	];
+
 	return (
 		<>
-			<div id='layout-grid'>
-				<div id='left-toolbar'>
-					<div id='profile-bar'>
-						<ul id='main-links'>
-							<li>{username}</li>
+			<Box sx={{ display: 'flex' }}>
 
-							<li className='main-page-link'>
-								<Link
-									className="main-link"
-									onClick={() => {
-										const logout = async () => {
-											try {
-												let logout_res = await axios.post(
-													`http://localhost:5000/auth_login/logout/`,
-													null,
-													{
-														withCredentials: true
-													})
+				<ThemeProvider
+					theme={createTheme({
+						components: {
+							MuiListItemButton: {
+								defaultProps: {
+									disableTouchRipple: true,
+								},
+							},
+						},
+						palette: {
+							mode: 'dark',
+							primary: { main: 'rgb(102, 157, 246)' },
+							background: { paper: 'rgb(5, 30, 52)' },
+						},
+					})}
+				>
 
-												if (logout_res.status === 200) {
-													navigate("/");
-												}
-											} catch (err) {
-												console.error(err);
-											}
-										};
+					<Drawer
+						sx={{
+							flexShrink: 0,
+							width: drawerWidth,
+						}}
+						PaperProps={{
+							sx: {
+								boxSizing: 'border-box',
+								width: drawerWidth,
+							}
+						}}
+						variant="permanent"
+						anchor="left"
+					>
 
-										logout()
+						<CustomNav component="nav" disablePadding>
+							<ListItemButton component="a" href="#customized-list">
+								<ListItemIcon sx={{ fontSize: 20 }}>🔭</ListItemIcon>
+								<ListItemText
+									sx={{ my: 0 }}
+									primary="Empyrean"
+									primaryTypographyProps={{
+										fontSize: 20,
+										fontWeight: 'medium',
+										letterSpacing: 0,
 									}}
-								>Logout</Link>
-							</li>
+								/>
+							</ListItemButton>
 
-							<li className='main-page-link'>
-								<Link className="main-link" to="/registration">Register new user</Link>
-							</li>
-						</ul>
+							<Divider />
 
-					</div>
-					<p id='main-page-title'>MAIN PAGES</p>
-					<ul id='main-links'>
-						<li className='main-page-link'><Link className="main-link" to="/observation">Observe</Link></li>
-						<li className='main-page-link'><Link className="main-link" to="/logsheet">Logsheets</Link></li>
-					</ul>
-				</div>
-				<div id='main-content'>
-					<div id='page-content'><Outlet /></div>
-					<div>
-						<hr id="footer-line-break" />
-						<footer id='footer-section'>
-							<p id="left-info">
-								Designed by students from NAU's CEIAS capstone program
-							</p>
-							<span>
-								V0.0.2
-							</span>
-							<ul id="right-info">
-								<li className="footer-nav-link"><Link to="/about">About</Link></li>
-								<li className="footer-nav-link"><Link to="/contact">Contact</Link></li>
-							</ul>
-						</footer>
-					</div>
-				</div>
-			</div>
+							<ListItem component="div" disablePadding>
+								<ListItemButton sx={{
+									fontSize: 15,
+									fontWeight: 'medium',
+									height: 23,
+									mt: 5,
+								}}>
+
+									<ListItemIcon>
+										<Home color="primary" />
+									</ListItemIcon>
+
+									<ListItemText
+										primary={username}
+										primaryTypographyProps={{
+											color: 'primary',
+											fontWeight: 'medium',
+											variant: 'body2',
+										}}
+									/>
+
+								</ListItemButton>
+							</ListItem>
+
+							<CreateNavItems items={userItems} />
+
+							<Divider />
+							<Box
+								sx={{
+									bgcolor: 'rgba(71, 98, 130, 0.2)',
+									pb: 2,
+								}}
+							>
+
+								<ListItemText
+									primary="Navigation"
+									primaryTypographyProps={{
+										fontSize: 15,
+										fontWeight: 'medium',
+										mb: '2px',
+									}}
+									sx={{
+										my: 0,
+										px: 3,
+										pt: 2.5,
+										pb: 0,
+										'&:hover, &:focus': { '& svg': { opacity: 1 } },
+									}}
+								/>
+
+								<CreateNavItems items={navItems} />
+
+							</Box>
+						</CustomNav>
+					</Drawer>
+				</ThemeProvider>
+
+				<Box
+					component="main"
+					className="main-content"
+					sx={{ flexGrow: 1, p: 3 }}
+				>
+					<Outlet />
+					<Footer />
+				</Box>
+			</Box >
 		</>
-	)
+	);
 }
+
 
 export { Layout }
