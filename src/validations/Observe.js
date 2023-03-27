04a9f2aa46f1d0254a4228cb3bc3a7587a95d16b
McAuley Penney <jacobmpenney@gmail.com>
2023-03-27 13:58:07 -0700

    fix: Incorrect observation form validation regex
    The validation regex for the observation form
    did not catch values that were appended, e.g. a
    value could initially be correct, but the user
    could make it incorrect by adding items of any
    type to the end of it and the regex would allow
    it.

--- src/validations/Observe.js
+++ src/validations/Observe.js
@@ -6,14 +6,14 @@ const requestFieldsRegex = {
 
 	// from 00:00:00.00 to 24:00:00, with max
 	// values for each segment being 24:59:59.99
-	right_ascension: new RegExp('^([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9](.[0-9][0-9])?|24:00:00(.00)?$'),
+	right_ascension: new RegExp("^(([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9](.[0-9][0-9])?|24:00:00(.00)?)$"),
 
 	// from -90:00:00.00 to 90:00:00.00, with max
 	// values for each segment being 90:59:59:99
-	declination: new RegExp("^0|[+-][0-8][0-9]:[0-5][0-9]:[0-5][0-9](.[0-9][0-9])?|90:00:00(.00)?$"),
+	declination: new RegExp("^(0|[+-][0-8][0-9]:[0-5][0-9]:[0-5][0-9](.[0-9][0-9])?|90:00:00(.00)?)$"),
 
 	// float, with two places, from -90.00 to 90.00
-	altitude: new RegExp("^-?(([0-8][0-9]?|9)(.[0-9][0-9])?|90(.00)?)"),
+	altitude: new RegExp("^(-?(([0-8][0-9]?|9)(.[0-9][0-9])?|90(.00)?))$"),
 }
 
 const requestFormSchema = yup.object().shape({
