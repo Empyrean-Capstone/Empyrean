b3f45e1938eb35a5104fff938836e87a1fe01768
McAuley Penney <jacobmpenney@gmail.com>
2023-04-01 09:23:18 -0700

    feat(User): add personal name and role to User model
    We want to be able to access the personal name of
    the user to autofill certain data, such as the
    "OBSERVER" header in fits files. We also want to
    provide roles so that users can be provided admin
    access.

--- api/main/models/user.py
+++ api/main/models/user.py
@@ -1,12 +1,17 @@
 from main import db, app
 
+
 class User(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = "user"
 
-    id = db.Column( db.Integer, primary_key = True )
-    username = db.Column( db.String() )
-    password = db.Column( db.String() )
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String())
+    name = db.Column(db.String())
+    password = db.Column(db.String())
+    isadmin = db.Column(db.Boolean)
 
-    def __init__(self, username, password):
+    def __init__(self, username: str, name: str, password: str, isAdmin: bool):
         self.username = username
+        self.name = name
         self.password = password
+        self.isadmin = isAdmin
