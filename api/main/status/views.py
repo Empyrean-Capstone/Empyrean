c3c66075133f6f72585e0f03ee337aa73ea333a1
koseward <kos34@nau.edu>
2023-04-05 19:29:00 -0700

    chore: added docstrings and comments to the status/views page

--- api/main/status/views.py
+++ api/main/status/views.py
@@ -9,13 +9,33 @@ from ..models import Status, Instrument
 
 # FIXME: must have a way to designate the current camera
 def get_current_camera() -> int:
+    """
+    Gets the most recent camera in use
+    
+    Returns:
+    --------
+        int:
+            The indentifier of the camera currently in use
+    """
     return Instrument.query.filter_by(instrumentName="ZWO ASI120MM-S").first().instrumentId
 
 
 def get_current_obsid() -> int:
+    """
+    Finds the status of the current camera, looking for the observation id 
+    of the current observation
+    
+    Returns:
+    --------
+        int
+            The id of the current observation in the eyes of the database
+    """
     cur_camera_id: int = get_current_camera()
 
-    cur_camera_status = Status.query.filter_by(instrumentID=cur_camera_id, statusName="obs_id").first()
+    # There is a status that every camera must have and update which is
+    # the id of the observation being taken
+    cur_camera_status = Status.query.filter_by(
+        instrumentID=cur_camera_id, statusName="obs_id").first()
 
     cur_obsid: int = int(cur_camera_status.statusValue)
 
@@ -23,13 +43,36 @@ def get_current_obsid() -> int:
 
 @status.get('/index')
 def index():
+    """
+    Returns all of the statuses in the database.
+    Used on the frontpage for management. These are the values on load which are 
+    then updated in real time
+    
+    Returns: 
+    --------
+        list
+            List of all of the serialized statuses. A serialized status is a 
+            simplified version of the status, only including what is needed for
+            the status table to preset the data. See the /main/models/status.py
+            for details on what the serialize function does.
+    """
     result = Status.query.all()
     for index in range(len(result)):
         result[index] = result[index].serialize()
     return result
 
 @sio.on("get_instrument_id")
-def get_instrument_id(instrument_name):
+def get_instrument_id(instrument_name) -> int:
+    """
+    On the bootup of an instrument to be connected to this app, it must obtain
+    its database identification number from the database. This provides the id,
+    where if that instrument has been connected before, its database id is returned
+    but if not, a new instrument is created and that id is returned
+
+    Returns:
+        int
+            The database id of the instrument that made the request. 
+    """
     # make query to recieve the id of the requested object
     instrument = Instrument.query.filter_by(instrumentName=instrument_name).first()
 
@@ -46,6 +89,12 @@ def get_instrument_id(instrument_name):
 
 @sio.on("update_status")
 def update_status(instrument_id, update_dict):
+    """
+    From the instruments, updates that instrument's statuses as they perform
+    work. 
+    
+    Parameters:
+        instrument_id : int """
     for key, value in update_dict.items():
         status = Status.query.filter_by(instrumentID=instrument_id, statusName=key).first()
 
@@ -66,4 +115,8 @@ def update_status(instrument_id, update_dict):
 
 @sio.on("observation_complete")
 def update_request_form():
+    """
+    On completion of an observation, this emit allows for more observations
+    to be made. 
+    """
     sio.emit("enable_request_form")
