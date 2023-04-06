3454419cfbc51048308b147860eefb2c62e0a8d3
koseward <kos34@nau.edu>
2023-04-05 21:17:34 -0700

    Created docstrings for the status model

--- api/main/models/status.py
+++ api/main/models/status.py
@@ -2,6 +2,21 @@ from main import db, app
 import json
 
 class Status(db.Model):
+    """
+    The python representation of the Status table of the database 
+    
+    Attributes:
+    -----------
+        id : int
+            identification number in the database
+        instrumentID : int
+            Foreign key from the owner instrument
+        statusName : str
+            Name of the status being kept track of
+        statusValue : str
+            Value of the status
+    """
+    
     __tablename__ = 'status'
 
     id = db.Column( db.Integer, primary_key = True )
@@ -10,11 +25,24 @@ class Status(db.Model):
     statusValue = db.Column( db.String() )
 
     def __init__(self, instrumentID, statusName, statusValue):
+        """
+        Sets the value of the status default values
+        """
+        
         self.instrumentID = instrumentID
         self.statusName = statusName
         self.statusValue = statusValue
 
     def serialize( self ):
+        """
+        Returns the status in a way that the frontend can use
+        
+        Returns: 
+        --------
+            dict:
+                Values of the status to be read by the frontend
+        """
+        
         return {"id": self.id,
                 "instrumentID": self.instrumentID,
                 "statusName": self.statusName,
