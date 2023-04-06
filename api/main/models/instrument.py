f744875617edac0cfd697749dedac5c6b8c56396
koseward <kos34@nau.edu>
2023-04-06 09:03:17 -0700

    chore: Created docstring for the instrument model class

--- api/main/models/instrument.py
+++ api/main/models/instrument.py
@@ -1,13 +1,32 @@
 from main import db
 
 class Instrument( db.Model ):
+    """
+    The python object implementation of the Instrument table of the database
+    
+    Attributes:
+    -----------
+        instrumentID : int 
+            Identifier of the instrument
+        instrumentName : str
+            The name of the instrument
+    """
+    
     __tablename__ = 'instrument'
 
     instrumentId = db.Column( db.Integer, primary_key = True )
     instrumentName = db.Column( db.String() )
 
     def __init__( self, instrumentName ):
+        """
+        Initializes the name of the instrument
+        """
+        
         self.instrumentName = instrumentName
 
     def __repr__(self):
+        """
+        Allows for a printable representation of the given instrument
+        """
+        
         return 'ID: {}. \n Name: {}'.format( self.instrumentId, self.instrumentName )
